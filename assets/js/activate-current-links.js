6b3cd0ec703c8a0bfa7b45f775c714771ec8357c
John Williams <john@thudfactor.com>
2018-08-03 12:21:44 -0400

    Rename the the color schemes to something more descriptive

--- assets/js/activate-current-links.js
+++ assets/js/activate-current-links.js
@@ -4,7 +4,7 @@ Drupal.behaviors.activate_current_links = {
   attach: (context, settings) => {
     $('a').each(function() {
       $a = $(this);
-      url = $a.attr('href')
+      url = $a.attr('href');
       if (url == window.location.href || url == window.location.pathname) {
         $a.addClass('is-active');
       }
