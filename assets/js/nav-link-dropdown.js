3ac73030b6d8f6ba361edc2005f0c299243f0af6
Andrew Shooner <andrew.shooner@uky.edu>
2019-08-10 20:07:38 -0400

    Additional mega menu adjustments for font size, etc

--- assets/js/nav-link-dropdown.js
+++ assets/js/nav-link-dropdown.js
@@ -1,33 +1,42 @@
 const largeScreenSize = 927;
 
 $('.mega-menu-nav-item').each(function() {
-  $(this).on('click', function() {
-    var $slabEl = $(this).next('.mega-menu__slab');
-    var navElement = document.getElementById('MainNav');
-    var submenuMargin = Math.ceil((window.innerWidth - navElement.offsetWidth) / 2);
 
-    if ($slabEl.hasClass('open')) {
-      $slabEl.removeClass('open');
-      if (window.innerWidth > largeScreenSize) {
-        $slabEl.css({
-          'padding': '0',
-          'left': `-${submenuMargin}px`
-        });
-      }
-    } else {
-      $slabEl.addClass('open');
-      if (window.innerWidth > largeScreenSize) {
-        $('.mega-menu__slab').not($slabEl).removeClass('open').css({
-          'padding': '0',
-          'left': `-${submenuMargin}px`
-        });
-        $slabEl.css({
-          'padding': `1.5rem ${submenuMargin}px`,
-          'left': `-${submenuMargin}px`
-        });
-      }
-    }
-  });
+    $(this).on('click', function() {
+        var $slabEl = $(this).next('.mega-menu__slab');
+        var navElement = document.getElementById('MainNav');
+        var submenuMargin = Math.ceil((window.innerWidth - navElement.offsetWidth) / 2);
+
+        if ($slabEl.hasClass('open')) {
+            $slabEl.removeClass('open');
+            if (window.innerWidth > largeScreenSize) {
+                $slabEl.css({
+                    'padding': '0',
+                    'left': `-${submenuMargin}px`
+                });
+            }
+        } else {
+            $slabEl.addClass('open');
+            if (window.innerWidth > largeScreenSize) {
+                $('.mega-menu__slab').not($slabEl).removeClass('open').css({
+                    'padding': '0',
+                    'left': `-${submenuMargin}px`
+                });
+                $slabEl.css({
+                    'padding': `1.5rem ${submenuMargin}px`,
+                    'left': `-${submenuMargin}px`
+                });
+            }
+        }
+    });
+
+    $(this).on('keydown', function() {
+        var $slabEl = $(this).next('.mega-menu__slab');
+        if (!$slabEl.hasClass('open')) {
+          $(this).click();
+        }
+    });
+
 });
 
 $(window).on('click', function(e) {
