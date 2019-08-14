83cb3209b8284bb666eea42eeb048a59f158b3f8
Andrew Shooner <andrew.shooner@uky.edu>
2019-08-14 16:31:49 -0400

    Correct dropdown js issue

--- assets/js/link-group-dropdown.js
+++ assets/js/link-group-dropdown.js
@@ -21,4 +21,14 @@ const link_group_dropdown = () => {
 
     });
 
+
+    $(window).on('click', function(e) {
+
+        // if the target was not within the expanded dropdown
+        console.log($(e.target).parents('.link-group-dropdown'));
+        if ($(e.target).parents('.link-group-dropdown').length < 1){
+            $('.link-group-dropdown[aria-expanded="true"]').attr('aria-expanded', 'false');
+        }
+    });
+
 }
