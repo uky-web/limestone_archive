633f5d09ecbc48cc067c9be73bb3cb88a8803c73
Andrew Shooner <andrew.shooner@uky.edu>
2023-02-09 20:49:31 +0000

    Update watches; delete public-bk files

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -8,11 +8,11 @@ buildJS = require('./buildJS'),
 buildCSS = require('./buildCSS'),
 buildImages = require('./buildImages'),
 buildIconfont = require('./buildIconfont');
-
-
+const { exec } = require('node:child_process')
+const { spawn } = require('node:child_process')
 
 const watcher = function(cb){
-    watch(paths.sass.concat(paths.font_sass_tpl), {delay:400}, buildCSS);
+    watch(paths.sass.concat(paths.font_sass_tpl),buildCSS);
     watch(paths.js, buildJS);
     watch(paths.lib, buildLibs);
     watch(paths.images, buildImages);
@@ -37,45 +37,64 @@ const build = series(
 // export public gulp tasks
 module.exports = { 
     build,
-    start: series(build, watcher),
+    // start: series(build, watcher),
     watch: watcher
 };
 
 
 /*** Patternlab gulp tooling ********
  * If patternlab is refactored into a workspace or other 
- * managed sub-package this tolling should go with it.
+ * managed sub-package this tooling should go with it.
  */
 
-//// Patternlab-specific tasks
 
 const {buildPatternlabFiles, buildPatternlabPages, buildPatternlab} = require('./buildPatternlab');
 const {servePatternlab} = require('./servePatternlab');
 const patternlabPaths = {
     twig: "./patternlab/source/_patterns/**/*.twig",
-    css:"./patternlab/public/css"
+    css:"./patternlab/public/css",
+    js:"./patternlab/public/js",
+    fonts: "./patternlab/public/fonts",
+    images:"./patternlab/public/images"
 }
 
 
-const watchPatternlab = function(){
+const watchPatternlab = function(cb){
     const browserSync = require('browser-sync').get('patternlab');
-    watch(paths.dist_css, {delay:400}, function movePatternlabCSS(cb){
+    watch(paths.dist_css, function movePatternlabCSS(cb){
       return  src(paths.dist_css+"/*")
         .pipe(dest(patternlabPaths.css))
         .pipe(browserSync.stream()); 
        
     })
 
-    watch(patternlabPaths.twig, function moveFiles(){
-        const browserSync = require('browser-sync').get('patternlab');
-        buildPatternlabFiles();
-        browserSync.reload();
-    })
+    watch(paths.dist_js, function movePatternlabJS(cb){
+        return  src(paths.dist_js+"/*")
+          .pipe(dest(patternlabPaths.js))
+          .pipe(browserSync.stream()); 
+      })
 
+    watch(paths.dist_fonts, function movePatternlabFonts(cb){
+        return  src(paths.dist_fonts+"/*")
+          .pipe(dest(patternlabPaths.fonts))
+          .pipe(browserSync.stream()); 
+      })
+
+    watch(paths.dist_images, function movePatternlabImages(cb){
+        return  src(paths.dist_images+"/*")
+          .pipe(dest(patternlabPaths.images))
+          .pipe(browserSync.stream()); 
+      })
+
+    watch(patternlabPaths.twig, function moveFiles(cb){
+        const browserSync = require('browser-sync').get('patternlab');
+        buildPatternlabPages(function(){
+            browserSync.reload();
+            cb();
+        });
+    })
+    cb()
 }
 
+module.exports.start = series(build, watcher, buildPatternlab,servePatternlab, watchPatternlab);
 
-
-module.exports.startPatternlab = series(buildPatternlab,servePatternlab, watchPatternlab);
-
-module.exports.startAll = series(build, watcher, buildPatternlab,servePatternlab, watchPatternlab)
